ff05969c479fe5c9478c9a6588d814fb896faebf
Den4562 <119700990+Den4562@users.noreply.github.com>
2024-04-26 15:16:01 +0300

    Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import s from "../styles/header.module.css";
+import { GiDeliveryDrone } from "react-icons/gi";
+import present_drone from "../assets/2.png";
+
+const Header = () => {
+  return (
+    <div className={s.container}>
+      <GiDeliveryDrone
+        style={{
+          position: "absolute",
+          fontSize: "55px",
+          marginTop: "10px",
+          marginLeft: "120px",
+        }}
+      />
+      <div className={s.nav_text}>
+        <p>HOME</p>
+        <p>GALLERY</p>
+        <p>PRODUCTS</p>
+        <p>REVIEW</p>
+        <p>SUPPORT</p>
+      </div>
+
+      <div className={s.present_text}>
+        <h2>Representing Drone Light </h2>
+        <div className={s.present_text_drone}>
+          Look up the sky and beautiful world with simple navigation. <br />
+          Just record anmd get a lot memories to share, lighlty and fast like a
+          lightning
+        </div>
+        <button>Get now</button>
+        <div>
+          <img
+            style={{
+              marginTop: "20px",
+              width: "500px",
+              height: "auto",
+            }}
+            src={present_drone}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header.jsx";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Main;
